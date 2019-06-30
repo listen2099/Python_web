dd86d13d2b7905eab7f477177f476e5cadece0ac
listen2099 <listen2099@gmail.com>
2019-06-30 15:29:42 +0800

    ok

--- like_flask.py
+++ like_flask.py
@@ -1,19 +1,23 @@
 from wsgiref.simple_server import make_server
+import cgi
+from urllib.parse import parse_qs
 
+# 127.0.0.1:9000?id=1&name=tom&age=20
 
 def simple_app(environ, start_response):  # 这两个参数是自动注入的
-    # print(environ)
-    for k, v in sorted(environ.items()):
-        print(k, v)
-    print('-' * 30)
-
+    query_string = environ.get('QUERY_STRING')  # 查询字符串
+    print(query_string)
+    # qs = {k: v for k, _, v in map(lambda x: x.partition('='), query_string.split('&'))}
+    # qs = cgi.parse_qs(query_string)  # cgi已过期
+    # print(qs)
+    qs = parse_qs(query_string)
+    print(qs)
     status = '200 OK'
     headers = [('Content-type', 'text/plain; charset=utf-8')]
 
     start_response(status, headers)  # 在返回正文之前首先要返回状态码,报文头
 
-    ret = [("%s: %s\n" % (key, value)).encode("utf-8")
-           for key, value in environ.items()]
+    ret = [query_string.encode('utf-8')]
     return ret  # 这里return的是正文,必须是个可迭代对象,一般是列表,可以是一个字符串元素
 
 
